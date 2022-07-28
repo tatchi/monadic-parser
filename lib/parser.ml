99b29d3ad7c8e232fc957104cbf8ed73747ef5e1
Corentin Leruth <corentin.leruth@gmail.com>
2022-07-28 22:28:49 +0200

    simplify encoding ?

--- lib/parser.ml
+++ lib/parser.ml
@@ -50,9 +50,9 @@ module Input = struct
     (rest, input)
 end
 
-type 'a t = { run : Input.t -> Input.t * ('a, Error.t) result }
+type 'a t = Input.t -> Input.t * ('a, Error.t) result
 
-let return t = { run = (fun input -> (input, Ok t)) }
+let return t input = (input, Ok t)
 
 let is_number = function
   | '0' .. '9' -> true
@@ -68,162 +68,117 @@ let is_number = function
    in
    loop [] n *)
 
-let int n =
-  { run =
-      (fun input ->
-        let s = string_of_int n in
-        let unexpected_int_error =
-          Error.create (Printf.sprintf "Expected number %i" n) input.pos
-        in
-        match Input.lsplit ~prefix:s input with
-        | None -> (input, Error unexpected_int_error)
-        | Some (input', prefix_input) ->
-          if prefix_input.text = s then (input', Ok n)
-          else (input, Error unexpected_int_error))
-  }
+let int n (input : Input.t) =
+  let s = string_of_int n in
+  let unexpected_int_error =
+    Error.create (Printf.sprintf "Expected number %i" n) input.pos
+  in
+  match Input.lsplit ~prefix:s input with
+  | None -> (input, Error unexpected_int_error)
+  | Some (input', prefix_input) ->
+    if prefix_input.text = s then (input', Ok n)
+    else (input, Error unexpected_int_error)
 
-let end_ =
-  { run =
-      (fun input ->
-        if String.length input.text = 0 then (input, Ok ())
-        else
-          ( input
-          , Error
-              (Error.create
-                 (Printf.sprintf "Expected end of input, instead got: `%s`"
-                    input.text)
-                 input.pos) ))
-  }
+let end_ (input : Input.t) =
+  if String.length input.text = 0 then (input, Ok ())
+  else
+    ( input
+    , Error
+        (Error.create
+           (Printf.sprintf "Expected end of input, instead got: `%s`" input.text)
+           input.pos) )
 
-let string s =
-  { run =
-      (fun input ->
-        let unexpected_prefix_error =
-          Error.create (Printf.sprintf "Expected string `%s`" s) input.pos
-        in
-        match Input.lsplit ~prefix:s input with
-        | None -> (input, Error unexpected_prefix_error)
-        | Some (input', prefix_input) ->
-          if prefix_input.text = s then (input', Ok s)
-          else (input, Error unexpected_prefix_error))
-  }
+let string s (input : Input.t) =
+  let unexpected_prefix_error =
+    Error.create (Printf.sprintf "Expected string `%s`" s) input.pos
+  in
+  match Input.lsplit ~prefix:s input with
+  | None -> (input, Error unexpected_prefix_error)
+  | Some (input', prefix_input) ->
+    if prefix_input.text = s then (input', Ok s)
+    else (input, Error unexpected_prefix_error)
 
-let map p ~f =
-  { run =
-      (fun input ->
-        match p.run input with
-        | (_, Error _) as e -> e
-        | input, Ok r -> (input, Ok (f r)))
-  }
+let map p ~f input =
+  match p input with
+  | (_, Error _) as e -> e
+  | input, Ok r -> (input, Ok (f r))
 
-let bind p ~f =
-  { run =
-      (fun input ->
-        match p.run input with
-        | (_, Error _) as e -> e
-        | input, Ok r -> (f r).run input)
-  }
+let bind p ~f input =
+  match p input with
+  | (_, Error _) as e -> e
+  | input, Ok r -> (f r) input
 
-let both p1 p2 =
-  { run =
-      (fun input ->
-        match p1.run input with
-        | (_, Error _) as e -> e
-        | input, Ok r1 -> (
-          match p2.run input with
-          | (_, Error _) as e -> e
-          | input, Ok r2 -> (input, Ok (r1, r2))))
-  }
+let both p1 p2 input =
+  match p1 input with
+  | (_, Error _) as e -> e
+  | input, Ok r1 -> (
+    match p2 input with
+    | (_, Error _) as e -> e
+    | input, Ok r2 -> (input, Ok (r1, r2)))
 
-let right p1 p2 =
-  { run =
-      (fun input ->
-        match p1.run input with
-        | (_, Error _) as e -> e
-        | input', Ok _ -> p2.run input')
-  }
+let right p1 p2 input =
+  match p1 input with
+  | (_, Error _) as e -> e
+  | input', Ok _ -> p2 input'
 
-let left p1 p2 =
-  { run =
-      (fun input ->
-        match p1.run input with
-        | (_, Error _) as e -> e
-        | input', Ok r1 -> (
-          match p2.run input' with
-          | (_, Error _) as e -> e
-          | input'', Ok _ -> (input'', Ok r1)))
-  }
+let left p1 p2 input =
+  match p1 input with
+  | (_, Error _) as e -> e
+  | input', Ok r1 -> (
+    match p2 input' with
+    | (_, Error _) as e -> e
+    | input'', Ok _ -> (input'', Ok r1))
 
-let either p1 p2 =
-  { run =
-      (fun input ->
-        match p1.run input with
-        | (_, Ok _) as r -> r
-        | _, Error left_error -> (
-          match p2.run input with
-          | (_, Ok _) as r -> r
-          | _, Error right_error ->
-            ( input
-            , Error
-                (Error.create
-                   (Printf.sprintf "%s or %s" left_error.desc right_error.desc)
-                   input.pos) )))
-  }
+let either p1 p2 (input : Input.t) =
+  match p1 input with
+  | (_, Ok _) as r -> r
+  | _, Error (left_error : Error.t) -> (
+    match p2 input with
+    | (_, Ok _) as r -> r
+    | _, Error (right_error : Error.t) ->
+      ( input
+      , Error
+          (Error.create
+             (Printf.sprintf "%s or %s" left_error.desc right_error.desc)
+             input.pos) ))
 
-let bool b =
-  { run =
-      (fun input ->
-        let p = either (string "true") (string "false") in
-        match p.run input with
-        | input', Ok r when bool_of_string r = b -> (input', Ok b)
-        | input', Error _ | input', Ok _ ->
-          ( input'
-          , Error
-              (Error.create (Printf.sprintf "Expected bool `%b`" b) input.pos)
-          ))
-  }
+let bool b input =
+  let p = either (string "true") (string "false") in
+  match p input with
+  | input', Ok r when bool_of_string r = b -> (input', Ok b)
+  | input', Error _ | input', Ok _ ->
+    ( input'
+    , Error (Error.create (Printf.sprintf "Expected bool `%b`" b) input.pos) )
 
-let parse_while p =
-  { run =
-      (fun input ->
-        let input', parsed = Input.take_while p input in
-        (input', Ok parsed.text))
-  }
+let parse_while p input =
+  let input', parsed = Input.take_while p input in
+  (input', Ok parsed.text)
 
-let optional p =
-  { run =
-      (fun input ->
-        match p.run input with
-        | _, Error _ -> (input, Ok None)
-        | input', Ok r -> (input', Ok (Some r)))
-  }
+let optional p input =
+  match p input with
+  | _, Error _ -> (input, Ok None)
+  | input', Ok r -> (input', Ok (Some r))
 
-let not p =
-  { run =
-      (fun input ->
-        match p.run input with
-        | _, Error _ -> (input, Ok ())
-        | _, Ok _ ->
-          ( input
-          , Error
-              (Error.create
-                 (* TODO: add a [to_string] fn in the type so that we can print what we parsed ? *)
-                 (Printf.sprintf "Expected not to parse `%s`" input.text)
-                 input.pos) ))
-  }
+let not p (input : Input.t) =
+  match p input with
+  | _, Error _ -> (input, Ok ())
+  | _, Ok _ ->
+    ( input
+    , Error
+        (Error.create
+           (* TODO: add a [to_string] fn in the type so that we can print what we parsed ? *)
+           (Printf.sprintf "Expected not to parse `%s`" input.text)
+           input.pos) )
 
 let space = string " "
 
-let many p =
-  { run =
-      (fun input ->
-        let rec loop acc (input : Input.t) =
-          match p.run input with
-          | _, Error _ -> (input, Ok (acc |> List.rev))
-          | input', Ok r -> loop (r :: acc) input'
-        in
-        loop [] input)
-  }
+let many p input =
+  let rec loop acc (input : Input.t) =
+    match p input with
+    | _, Error _ -> (input, Ok (acc |> List.rev))
+    | input', Ok r -> loop (r :: acc) input'
+  in
+  loop [] input
 
 module O = struct
   let ( let+ ) t f = map t ~f
@@ -242,5 +197,5 @@ module O = struct
 end
 
 let parse (s : string) (p : 'a t) =
-  let _, res = p.run (Input.of_string s) in
+  let _, res = p (Input.of_string s) in
   res
